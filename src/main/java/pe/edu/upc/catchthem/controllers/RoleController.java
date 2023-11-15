45283b1c1f1d996092c586eefaaf347cdbe10eeb
DanielHuerta03 <104838742+DanielHuerta03@users.noreply.github.com>
2023-11-14 20:01:03 -0500

    Update RoleController.java

--- src/main/java/pe/edu/upc/catchthem/controllers/RoleController.java
+++ src/main/java/pe/edu/upc/catchthem/controllers/RoleController.java
@@ -6,8 +6,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.catchthem.dtos.RoleDTO;
 import pe.edu.upc.catchthem.entities.Role;
+import pe.edu.upc.catchthem.entities.Users;
 import pe.edu.upc.catchthem.serviceInterfaces.IRoleService;
+import pe.edu.upc.catchthem.serviceInterfaces.IUsersService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +19,8 @@ import java.util.stream.Collectors;
 public class RoleController {
     @Autowired
     private IRoleService rS;
+    @Autowired
+    private IUsersService sS;
 
     @GetMapping
     @PreAuthorize("hasAuthority('ADMIN')")
@@ -26,14 +31,36 @@ public class RoleController {
         }).collect(Collectors.toList());
     }
     @PostMapping
-    public void registrar(@RequestBody RoleDTO roleDTO){
-        ModelMapper m= new ModelMapper();
-        Role r = m.map(roleDTO,Role.class);
-        rS.ingresar(r);
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('POLICIA')or hasAuthority('AGENTE')")
+    public void registrar(@RequestBody RoleDTO dto){
+        ModelMapper m = new ModelMapper();
+        Role r = m.map(dto,Role.class);
+        rS.insert(r);
     }
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public void eliminar(@PathVariable("id") Long id){
         rS.eliminar(id);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<RoleDTO> listarId(@PathVariable("id") long id) {
+        List<String[]> lista = rS.Buscarporidusuario(id);
+        List<RoleDTO>listadto=new ArrayList<>();
+        for(String[] data:lista){
+            RoleDTO dto =  new RoleDTO();
+            dto.setId(Long.parseLong(data[0]));
+            dto.setRol(data[1]);
+            dto.setUser(usuario(id));
+
+            listadto.add(dto);
+        }
+        return listadto;
+    }
+
+    public Users usuario(long id){
+        Users user = sS.listarId(id);
+        return user;
     }
 }
