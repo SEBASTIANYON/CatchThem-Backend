9a0bec59154dd4d59b5ddeef65fdf9b5b487458a
DanielHuerta03 <104838742+DanielHuerta03@users.noreply.github.com>
2023-11-14 20:03:13 -0500

    Update RoleRepository.java

--- src/main/java/pe/edu/upc/catchthem/repositories/RoleRepository.java
+++ src/main/java/pe/edu/upc/catchthem/repositories/RoleRepository.java
@@ -6,5 +6,6 @@ import pe.edu.upc.catchthem.entities.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
+    @Query(value = "select r.id, r.rol, r.user_id from roles r where r.user_id = :idusuario", nativeQuery = true)
+    public List<String[]> Buscarporidusuario(@Param("idusuario") long idusuario);
 }
