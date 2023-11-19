fea8d61c4b290916513c19e62f3927058df8f94e
DanielHuerta03 <104838742+DanielHuerta03@users.noreply.github.com>
2023-11-19 14:34:13 -0500

    Update ITipoEntidadService.java

--- src/main/java/pe/edu/upc/catchthem/serviceInterfaces/ITipoEntidadService.java
+++ src/main/java/pe/edu/upc/catchthem/serviceInterfaces/ITipoEntidadService.java
@@ -1,5 +1,7 @@
 package pe.edu.upc.catchthem.serviceInterfaces;
 
+import pe.edu.upc.catchthem.entities.Entidad;
+import pe.edu.upc.catchthem.entities.Sospechoso;
 import pe.edu.upc.catchthem.entities.TipoEntidad;
 
 import java.util.List;
@@ -10,5 +12,8 @@ public interface ITipoEntidadService {
     void delete(int id);
 
     List<TipoEntidad> list();
+
+
+    public TipoEntidad listarId(int id);
 
 }
